8fedf7c96b9bfc0f3f164a8f78024b3cbb8d0672
Ben Galloway <bdgalloway85@gmail.com>
2020-10-21 18:32:53 -0500

    weather api work

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,17 +7,31 @@ $('#input-button').on('click', function (event) {
     var inputCity = $('input').val();
     $('input').val("");
     console.log(inputCity);
-    getWeatherData(inputCity);
+    getCurrentWeatherData(inputCity);
 
 
 
 });
 
 //weather section
-function getWeatherData(city) {
-//console.log(city);
+function getCurrentWeatherData(city) {
 
-//renderWeatherData(response);
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?appid=16e2a29d08bf4766fcdb6563c3920b3d&q=' + city;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (res) {
+            console.log(res.coord.Lon);
+            var lon = res.coord.Lon;
+            var lat = res.coord.Lat;
+            console.log(lon);
+            console.log(lat);
+
+        })
+
+    //renderWeatherData(response);
 
 }
 
